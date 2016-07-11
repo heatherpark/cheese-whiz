f3a5f79f42c4cd163d0860f47b1476bb02a7a968
Heather Park <hthr.prk@gmail.com>
2016-07-11 16:06:37 -0700

    First commit

--- index.js
+++
@@ -1,7 +0,0 @@
-var app = require('./server.js');
-
-var port = 1337;
-
-app.listen(process.env.PORT || port);
-
-console.log('Server now listening on port ' + port);
---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+var port = 1337;
+
+app.listen(process.env.PORT || port);
+console.log('Server now listening on port ' + port);
+
+module.exports = app;
