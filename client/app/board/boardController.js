ee0a9741dd6ca2cfe19589b11493a6cfff8ea655
Heather Park <hthr.prk@gmail.com>
2016-07-12 00:29:35 -0700

    Implement class toggle only on clicked cards in board

--- client/app/board/boardController.js
+++ client/app/board/boardController.js
@@ -2,4 +2,12 @@ angular.module('memoryGame.board', [])
 
 .controller('BoardController', function($scope) {
   $scope.cards = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D'];
+  $scope.flippedClass = 'flipped';
+  $scope.toggleItem = {
+    index: -1
+  };
+  $scope.toggleClass = function(i) {
+    console.log(i);
+    $scope.toggleItem.index = i;
+  }
 });
