298bfd919d87d009288879c0933a97c686de71ab
Heather Park <hthr.prk@gmail.com>
2016-07-12 11:03:53 -0700

    Require Mongoose and configure Mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
-var firebase = require('firebase');
+var mongoose = require('mongoose');
 
 var app = express();
 
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/cheeseWhiz';
+
+mongoose.connect(mongoUri);
+
+var db = mongoose.connection;
+db.on('err', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+  console.log('Mongodb connection open');
+});
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
